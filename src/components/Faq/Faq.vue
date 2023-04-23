ac914d6df6847558b933f15c2148b5382d955f4c
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2023-04-23 21:31:51 +0700

    add some adjustment

--- src/components/Faq/Faq.vue
+++ src/components/Faq/Faq.vue
@@ -2,13 +2,20 @@
     <div class="faq-area ptb-100">
         <div class="container">
             <div class="row">
+                <div class="col-lg-4 col-md-12">
+                    <div class="faq-sidebar">
+                        <ul>
+                            <li><a href="faq"><span>instrumen</span></a></li>
+                            <li><a href="faq"><span>aplikasi</span></a></li>
+                        </ul>
+                    </div>
+                </div>
                 <div class="col-lg-8 col-md-12">
                     <div class="faq-accordion">
                         <accordion>
                             <accordion-item v-for="item in items" :key="item.id">
                                 <template v-slot:accordion-trigger>
                                 <button class="accordion-title" v-html="item.pertanyaan">
-                                    
                                 </button>
                                 </template>
                                 <template v-slot:accordion-content>
