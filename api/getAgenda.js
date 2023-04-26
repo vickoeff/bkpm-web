93f7443182c1e4466bfa9998f50b6e3df94ea909
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2023-04-26 18:02:17 +0700

    adjust get data function

--- api/getAgenda.js
+++ api/getAgenda.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 async function getAgenda() {
-    axios.defaults.baseURL = ''
-    const agenda = await axios.get('https://penkin-backend.denican.id/tanggal_kegiatan?sort=tanggal_mulai%20ASC');
+    const agenda = await axios({
+        method:'get',
+        url:'logout',
+        baseURL: 'https://penkin-backend.denican.id/tanggal_kegiatan?sort=tanggal_mulai%20ASC',
+       })
 
     return agenda
 }
