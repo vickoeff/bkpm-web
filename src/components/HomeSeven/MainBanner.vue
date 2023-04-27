a7b37d5d0c033e77fb8fce32d0d0a2d5c5fd907b
Vbreakin <vic.eff3@gmail.com>
2023-04-27 22:29:25 +0700

    change main color

--- src/components/HomeSeven/MainBanner.vue
+++ src/components/HomeSeven/MainBanner.vue
@@ -5,39 +5,59 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="new-app-main-banner-wrap-content">
                         <h1>Kementerian Investasi / BKPM</h1>
-                        <p>Badan Koordinasi Penanaman Modal adalah Lembaga Pemerintah Non Departemen Indonesia yang bertugas untuk merumuskan kebijakan pemerintah di bidang penanaman modal, baik dari dalam negeri maupun luar negeri.</p>
+                        <p>
+                            Badan Koordinasi Penanaman Modal adalah Lembaga
+                            Pemerintah Non Departemen Indonesia yang bertugas
+                            untuk merumuskan kebijakan pemerintah di bidang
+                            penanaman modal, baik dari dalam negeri maupun luar
+                            negeri.
+                        </p>
 
                         <div class="app-btn-box">
-                            <router-link to="/about" class="default-btn">Selengkapnya</router-link>
+                            <router-link to="/about" class="default-btn"
+                                >Selengkapnya</router-link
+                            >
                         </div>
                     </div>
-                </div> 
-                <div class="col-lg-6 col-md-12">
                 </div>
+                <div class="col-lg-6 col-md-12"></div>
             </div>
         </div>
 
-        <div v-if="!!slider.length" class="new-app-main-banner-wrap-image" data-aos="fade-left" data-aos-duration="2000">
+        <div
+            v-if="!!slider.length"
+            class="new-app-main-banner-wrap-image"
+            data-aos="fade-left"
+            data-aos-duration="2000"
+        >
             <div class="overlay"></div>
-            <carousel :items-to-show="1" :autoplay="2400" :transition="700" :wrapAround="true">
+            <carousel
+                :items-to-show="1"
+                :autoplay="2400"
+                :transition="700"
+                :wrapAround="true"
+            >
                 <slide v-for="slide in slider" :key="slide.id">
-                    <img class="slide-image" :src="slide.url_file" alt="">
+                    <img class="slide-image" :src="slide.url_file" alt="" />
                 </slide>
             </carousel>
         </div>
 
         <div class="new-app-main-banner-wrap-shape d-none d-md-block">
-            <img src="../../assets/images/more-home/banner/shape-5.png" alt="image">
+            <img
+                src="../../assets/images/more-home/banner/shape-5.png"
+                alt="image"
+            />
         </div>
     </div>
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue3-carousel'
-import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 
 export default {
-    name: 'MainBanner',
+    name: "MainBanner",
     components: {
         Carousel,
         Slide,
@@ -45,9 +65,8 @@ export default {
     props: {
         slider: {
             type: Array,
-            data: null
-        }
-    }
-}
+            data: null,
+        },
+    },
+};
 </script>
- 
