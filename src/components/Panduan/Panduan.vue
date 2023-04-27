78d9f22b6d4ba3daa109e9fcd2395c5979fac55a
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2023-04-28 00:03:50 +0700

    adjust content panduan page

--- src/components/Panduan/Panduan.vue
+++ src/components/Panduan/Panduan.vue
@@ -75,6 +75,166 @@
                     </div>
                 </div>
             </div>
+            <h2 style="text-align: center;">Panduan Penggunaan Sistem TIK Penilaian Kinerja</h2>
+            <div class="row justify-content-center">
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20PTSP%20Pemda%20(Web).pdf"
+                    style="margin: 1rem;"
+                >
+                    <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        PTSP Pemda
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20PTSP%20Pemda%20(Mobile).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        PTSP Pemda (Mobile) 
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20PPB%20K_L%20(Web).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        PPB K/L
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20PPB%20K_L%20(Mobile).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        PPB K/L (Mobile)
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20Verifikator%20(Web).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Verifikator
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20Verifikator%20(Mobile).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Verifikator (Mobile)
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20Penilaian%20Asosiasi%20Pengusaha%20(Web).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Penilaian Pemangku Kepentingan Organisasi Penguasa
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/Buku%20Panduan%20Penilaian%20Pemangku%20Kepentingan%20Organisasi%20Pengusaha%20(Mobile).pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Penilaian Pemangku Kepentingan Organisasi Penguasa
+                    </h4>
+                </a>
+            </div>
+            <h2 style="text-align: center;">Panduan Instrumen Penilaian</h2>
+            <div class="row justify-content-center">
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/panduan_instrumen/Paparan%20Workshop%20Penkin%202022.pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Paparan Workshop Penilaian Kinerja
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/panduan_instrumen/Paparan%20Workshop%20Penkin%202022_KL.pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Paparan Workshop Penilaian Kinerja K/L
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/panduan_instrumen/Penjelasan%20Instrumen%20Kinerja%20PPB%20Pemda.pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Penjelasan Instrumen Kinerja PBB Pemda
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/panduan_instrumen/Penjelasan%20Instrumen%20Kinerja%20PTSP.pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Penjelasan Instrumen Kinerja PTSP
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/panduan_instrumen/Penjelasan%20Instrumen%20Penilaian%20Provinsi%20Ke%20PemkabKota.pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Penjelasan Instrumen Pemangku Kepentingan Provinsi - Kab/Kota
+                    </h4>
+                </a>
+                <a
+                    className="gallery-item"
+                    class="btn btn-primary btn-lg col-lg-4 col-md-6 col-sm-6"
+                    href="https://penkin-backend.denican.id/public/dokumen/panduan_instrumen/Penjelasan%20Instrumen%20Kinerja%20PPB%20K-L.pdf"
+                    style="margin: 1rem;"
+                >
+                <i class="ri-book-open-line" style="color: white;"></i>
+                    <h4  style="color: white;" class="single-gallery-item">
+                        Penjelasan Instrumen Penilaian PBB K/L
+                    </h4>
+                </a>
+            </div>
         </div>
     </div>
 </template>
